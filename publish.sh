929473b675ffb01e6730f57fd2976d283fff9255
Troy Dai <troy.dai@outlook.com>
2018-01-04 19:13:58 -0800

    Update container registry

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
+registry=${1:-azureclidev}
+server=`az acr show -n $registry --query loginServer -otsv`
+
+az acr login -n $registry
+
 version=`cat version`
 for flavor in `ls dockerfiles`; do
-    docker build -t troydai/azurecli-a01-droid:$flavor-$version \
-                 -f dockerfiles/$flavor/Dockerfile \
-                 .
-    docker push troydai/azurecli-a01-droid:$flavor-$version
+    image=$server/azurecli-a01-droid:$flavor-$version
+    docker build -t $image -f dockerfiles/$flavor/Dockerfile .
+    docker push $image
 done
