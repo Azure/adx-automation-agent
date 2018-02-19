eae9af9fc02a940d00908d3d64d2f92ae2b07d25
Troy Dai <troy.dai@outlook.com>
2018-02-19 12:47:57 -0800

    Add commit to the build info

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 root=$(cd `dirname $0`; pwd)
 version=$1
+commit=${TRAVIS_COMMIT:-`git rev-parse --verify HEAD`}
 echo "Version: ${version:=`date -u +local-%Y%m%d-%H%M%S`}"
 
 for os in linux darwin windows; do
     export GOOS=$os
     mkdir -p $root/bin/$os
     echo -n "Building droid for $os ..."
-    go build -o $root/bin/$os/a01droid -ldflags "-X main.version=$version"
+    go build -o $root/bin/$os/a01droid -ldflags "-X main.version=$version -X main.sourceCommit=$commit"
     echo "Done."
 done
